163b7bcfefc895435cd9194d6c1758077b5cfda9
Diego Lago <diego.lago.gonzalez@gmail.com>
2018-02-21 17:07:46 +0100

    Simplest example raising a SIGSEGV

--- libtest.d
+++ libtest.d
@@ -4,16 +4,6 @@ import std.stdio;
 
 export void
 print_hello() {
-	writefln("Hello from function");
-}
-
-export class HelloPrinter {
-	this() {
-		writefln("HelloPrinter");
-	}
-
-	void printHello() {
-		writefln("Hello from class");
-	}
+	writefln("libtest.print_hello(): Hello!");
 }
 
